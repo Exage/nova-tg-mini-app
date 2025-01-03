d5791d53e4cf3edb4b1132802d29f239c4191048
Nikita Gorkavchuk <horkavchuk.nik@gmail.com>
2025-01-03 19:15:24 +0300

    update project structure

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,34 +1,29 @@
 import React from 'react'
 import styles from '@/Core.module.scss'
 
-import { UserAvatar } from '@/components/UserAvatar'
 import { BlockGradient } from '@/components/UI/BlockGradient'
 
+import { BlockTitle } from './components/BlockTitle'
+import { UserInfo } from './components/UserInfo'
+import { Points } from './components/Points'
+
 export const Profile = () => {
-    
     return (
         <div className={styles.wrapper}>
             <div className={'py-8'}>
 
-                <div className={'pt-12 flex flex-col items-center justify-center'}>
-
-                    <UserAvatar size={'w-32 h-32'} imageUrl={'/avatar-ph.png'} />
-
-                    <h2 className={'mt-4 text-4xl font-bold uppercase'}>
-                        MIKITAUIUX
-                    </h2>
-                    <h3 className={'text-base text-secondary-600 lowercase'}>
-                        #mikitauiux
-                    </h3>
-                
-                </div>
+                <UserInfo
+                    name={'MikitaUiUX'}
+                    username={'#mikitauiux'}
+                    imageUrl={'/avatar-ph.png'}
+                />
 
                 <div className={'grid grid-rows-2 grid-cols-2 gap-x-3 gap-y-4 mt-4'}>
-                
+
                     <BlockGradient>
                         <div className={'flex flex-col justify-around h-full'}>
-                            <h2 className={'capitalize text-accent-200 text-base font-medium'}>Current Points:</h2>
-                            <p className={'flex items-center gap-2 text-accent-500 after:bg-[url("/gem.svg")] after:inline-block after:bg-center after:bg-no-repeat after:bg-contain after:w-3 after:h-4'}>2000</p>
+                            <BlockTitle>Current Points:</BlockTitle>
+                            <Points>2000</Points>
                         </div>
                     </BlockGradient>
 
@@ -37,20 +32,20 @@ export const Profile = () => {
                             <img className={'w-full h-32 object-cover'} src="/ship-ph.jpg" alt="" />
                         </div>
                         <div className={'p-2 text-center'}>
-                            <h2 className={'capitalize text-accent-200 text-base font-medium'}>Favourite ship</h2>
+                            <BlockTitle>Favourite ship:</BlockTitle>
                             <p className={'font-medium text-xs text-white'}>Nebula Seeker</p>
                         </div>
                     </BlockGradient>
 
                     <BlockGradient>
                         <div className={'flex flex-col justify-around h-full'}>
-                            <h2 className={'capitalize text-accent-200 text-base font-medium'}>Total Points:</h2>
-                            <p className={'flex items-center gap-2 text-accent-500 after:bg-[url("/gem.svg")] after:inline-block after:bg-center after:bg-no-repeat after:bg-contain after:w-3 after:h-4'}>2000</p>
+                            <BlockTitle>Total Points:</BlockTitle>
+                            <Points>2000</Points>
                         </div>
                     </BlockGradient>
 
                     <BlockGradient className={'col-span-2'}>
-                        <h2 className={'capitalize text-accent-200 text-base font-medium'}>Ship Collection:</h2>
+                        <BlockTitle>Ship Collection:</BlockTitle>
 
                         <ul className={'mt-2.5 flex flex-col gap-2.5'}>
                             <li className={'flex justify-between items-center'}>
@@ -70,9 +65,8 @@ export const Profile = () => {
                                 <span className={'text-xs text-accent-200'}>At home</span>
                             </li>
                         </ul>
-
                     </BlockGradient>
-                
+
                 </div>
             </div>
         </div>
---
+++ src/pages/Profile/components/BlockTitle.jsx
@@ -0,0 +1,5 @@
+export const BlockTitle = ({ children }) => {
+    return (
+        <h2 className={'capitalize text-accent-200 text-base font-medium'}>{children}</h2>
+    )
+}
