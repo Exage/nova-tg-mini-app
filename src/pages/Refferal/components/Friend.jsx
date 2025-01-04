8765a8af816e59908e9c15186c3189749612fa4f
Nikita Gorkavchuk <horkavchuk.nik@gmail.com>
2025-01-04 20:34:27 +0300

    Update Friend.jsx

--- src/pages/Refferal/components/Friend.jsx
+++ src/pages/Refferal/components/Friend.jsx
@@ -14,10 +14,10 @@ export const Friend = ({ data }) => {
             </div>
             <div className={'ml-5 overflow-hidden flex-1'}>
                 <h3 className={'text-white text-xl font-bold text-ellipsis overflow-hidden'}>{data.name}</h3>
-                <p className={'flex items-center gap-1 text-gray-200 text-sm mt-1'}>
+                <div className={'flex items-center gap-1 text-gray-200 text-sm mt-1'}>
                     <ReactSVG src={friendsIcon} />
                     +1
-                </p>
+                </div>
             </div>
             <div className={classNames('text-xl font-bold', 'after:w-7 after:h-8 after:align-bottom after:ml-2', styles.gemAfter)}>
                 {data.gems}
