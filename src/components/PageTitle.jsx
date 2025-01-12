fe235224ff1682e2b43dc19630bfedc9652d147c
Nikita Gorkavchuk <horkavchuk.nik@gmail.com>
2025-01-12 13:14:37 +0300

    Update PageTitle.jsx

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,3 +1,3 @@
 export const PageTitle = ({ children }) => {
-    return <h1 className={'font-bold text-4xl text-white'}>{children}</h1>
+    return <h1 className={'font-bold text-4xl text-white min-h-10'}>{children}</h1>
 }
